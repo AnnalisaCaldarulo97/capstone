bc0b74b763e5688a4f74a6fe6fb28c81c05171fc
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-07-21 14:20:49 +0200

    base header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,24 @@
 function Header() {
     return (
-        <header></header>
+        <header className="py-4">
+            <article className="container ">
+                <div className="row">
+                    <div className="col-12 col-md-6 text-start">
+                        {/* <div className="title-container"> */}
+                        <h1 className="title title-font yellow">Little Lemon</h1>
+                        <h2 className="subtitle title-font light">Chicago</h2>
+                        {/* </div> */}
+                        <p className="text-font light description ">We are a family owned <br></br> Mediterranean restaurant,  <br></br>focused on traditional  <br></br> recipes served with a modern <br></br> twist</p>
+                        <div className="btn-container d-flex flex-column justify-content-end ">
+                            <a href="#" className="btn-yellow">Reserve a Table</a>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 d-flex justify-content-center">
+                        <img src="/media/restauranfood.jpg" className="header-img " alt="restaurant food" />
+                    </div>
+                </div>
+            </article>
+        </header>
     )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 function Nav() {
     return (
         <nav>
-            <div className="container d-flex align-items-center  py-3 justify-content-around">
+            <div className="container d-flex align-items-center py-3 justify-content-around">
                 <a className="navbar-brand me-auto">
-                    <img src="/media/logo_nav.png" alt="logo" className="d-inline-block align-text-top" width='200px' />
+                    <img src="/media/logo_nav.png" alt="logo" className="d-inline-block align-text-top" width='220px' />
                 </a>
                 <ul className="d-flex justify-content-around  list-unstyled pt-3 w-75 text-font">
                     <li className="nav-item"> <a href="#" className="nav-link">Home</a> </li>
