5da2390d3c8d8e4a46eeec3a15450c0ce3eb84cb
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-07-21 12:39:39 +0200

    navbar component done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Header  from './components/Header';
-import Nav  from './components/Nav';
-import Main  from './components/Main';
-import Footer  from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <meta name="language" content="english"/> 
-      <meta name="rating" content="safe for kids"/> 
-      <meta name="description" content="Indulge in fresh, flavorful dishes at Little Lemon! Explore our seasonal menu, all prepared with locally sourced ingredients. Make a reservation online or call us for a delightful dining experience."/>
-      <meta name="og:title" content="Little Lemon Restaurant"/>
-      <meta name="og:description" content="Indulge in fresh, flavorful dishes at Little Lemon! Explore our seasonal menu, all prepared with locally sourced ingredients. Make a reservation online or call us for a delightful dining experience."/>
-      <meta name="og:image" content=""/>
       <Nav />
       <Header />
       <Main />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,19 @@
 function Nav() {
     return (
         <nav>
-            <ul className="d-flex justify-content-around list-unstyled">
-                <li> <a href="#">Home</a> </li>
-                <li> <a href="#">About</a> </li>
-                <li> <a href="#">Menu</a> </li>
-                <li> <a href="#">Reservations</a> </li>
-                <li> <a href="#">Order online</a> </li>
-                <li> <a href="#">Login</a> </li>
-            </ul>
+            <div className="container d-flex align-items-center  py-3 justify-content-around">
+                <a className="navbar-brand me-auto">
+                    <img src="/media/logo_nav.png" alt="logo" className="d-inline-block align-text-top" width='200px' />
+                </a>
+                <ul className="d-flex justify-content-around  list-unstyled pt-3 w-75 text-font">
+                    <li className="nav-item"> <a href="#" className="nav-link">Home</a> </li>
+                    <li className="nav-item"> <a href="#" className="nav-link">About</a> </li>
+                    <li className="nav-item"> <a href="#" className="nav-link">Menu</a> </li>
+                    <li className="nav-item"> <a href="#" className="nav-link">Reservations</a> </li>
+                    <li className="nav-item"> <a href="#" className="nav-link">Order online</a> </li>
+                    <li className="nav-item"> <a href="#" className="nav-link">Login</a> </li>
+                </ul>
+            </div>
         </nav>
     )
 }
