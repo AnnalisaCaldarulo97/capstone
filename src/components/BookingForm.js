0f00ed24b7786f7a9936a2bb7c416cf5834900dd
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-07-26 16:07:19 +0200

    state lifted in form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,31 +1,47 @@
 import InputGroup from "./fragments/InputGroup";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function BookingForm() {
-    const [formData, setFormData] = useState({
-        resDate: '',
-        resTime: '',
-        guests: '',
-        occasion: '',
-    });
+function BookingForm({ formData, updateFormData }) {
     const handleSubmit = (e) => {
-        e.preventDefault()
-        // useEffect(() => {
-            console.log(formData)
-        // })
-    }
-    const availableTimes = [{ time: '17:00', key: 1 }, { time: '18:00', key: 2 }, { time: '19:00', key: 3 }, { time: '20:00', key: 4 }, { time: '21:00', key: 5 }, { time: '22:00', key: 6 }];
+        e.preventDefault();
+        // Handle form submission logic here (e.g., API call)
+        console.log('Submitted form data:', formData);
+    };
+
+    const handleChange = (event) => {
+        updateFormData({ [event.target.name]: event.target.value });
+    };
+
+    const availableTimes = [
+        { time: '17:00', key: 1 },
+        { time: '18:00', key: 2 },
+        { time: '19:00', key: 3 },
+        { time: '20:00', key: 4 },
+        { time: '21:00', key: 5 },
+        { time: '22:00', key: 6 },
+    ];
 
 
     return (
         <form className="w-100" onSubmit={handleSubmit}>
             <div className="inputContainer mb-3 d-flex flex-column">
                 <label htmlFor="res-date">Choose date</label>
-                <input type="date" id="res-date" name="date" value={formData.resDate} onChange={(e) => setFormData({ ...formData, resDate: e.target.value })} />
+                <input
+                    type="date"
+                    id="res-date"
+                    name="resDate"
+                    value={formData.resDate}
+                    onChange={handleChange}
+                />
             </div>
             <div className="inputContainer mb-3 d-flex flex-column">
                 <label htmlFor="res-time">Choose time</label>
-                <select id="res-time" name="time" value={formData.resTime} onChange={(e) => setFormData({ ...formData, resTime: e.target.value })}>
+                <select
+                    id="res-time"
+                    name="resTime"
+                    value={formData.resTime}
+                    onChange={handleChange}
+                >
                     {availableTimes.map((time) => (
                         <option key={time.key} value={time.time}>
                             {time.time}
@@ -35,17 +51,19 @@ function BookingForm() {
             </div>
             <div className="inputContainer mb-3 d-flex flex-column">
                 <label htmlFor="guests">Number of guests</label>
-                <input type="number" placeholder="1" min="1" max="10" id="guests" name="guests" value={formData.guests} onChange={(e) => setFormData({ ...formData, guests: e.target.value })} />
+                <input type="number" placeholder="1" min="1" max="10" id="guests" name="guests" value={formData.guests} onChange={handleChange} />
             </div>
             <div className="inputContainer mb-3 d-flex flex-column">
                 <label htmlFor="occasion">Occasion</label>
-                <select id="occasion" name="occasion" value={formData.occasion} onChange={(e) => setFormData({ ...formData, occasion: e.target.value })} defaultValue={'Other'}>
+                <select id="occasion" name="occasion" value={formData.occasion} onChange={handleChange} >
                     <option>Birthday</option>
                     <option>Anniversary</option>
                     <option>Other</option>
                 </select>
             </div>
-            <button type="submit" value="" className="btn btn-dark" >Make Your reservation</button>
+            <button type="submit" value="" className="btn btn-dark">
+                Make Your reservation
+            </button>
         </form>
     )
 }
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,6 +1,18 @@
 import BookingForm from "./BookingForm";
+import { useState } from 'react';
 
 function BookingPage() {
+    const [formData, setFormData] = useState({
+        resDate: '',
+        resTime: '',
+        guests: '',
+        occasion: '',
+    });
+
+    const updateFormData = (newData) => {
+        setFormData({ ...formData, ...newData });
+    };
+
     return (
         <div className="container-fluid container-booking">
             <div className="row justify-content-center">
@@ -8,7 +20,7 @@ function BookingPage() {
                     <img src="/media/restaurant.webp" className="img-fluid" alt="restaurant" />
                 </div>
                 <div className="col-12 col-md-5 d-flex align-items-center">
-                    <BookingForm />
+                    <BookingForm formData={formData} updateFormData={updateFormData} />
                 </div>
             </div>
         </div>
